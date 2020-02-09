b4e92256b79884d3fbd4728a847d92314eea408c
Kiran S <>
2020-02-09 14:51:44 -0500

    first commit

---
+++ src/main/java/join/queries/Application.java
@@ -0,0 +1,10 @@
+package join.queries;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
---
+++ src/main/java/join/queries/CustomerRepository.java
@@ -0,0 +1,15 @@
+package join.queries;
+
+import io.micronaut.data.annotation.Join;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.repository.CrudRepository;
+
+import java.util.Collection;
+
+@Repository
+@Join(value = "orders", type = Join.Type.FETCH)
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    // @Join(value = "orders", type = Join.Type.FETCH)
+    Collection<Customer> findByName(String name);
+}
